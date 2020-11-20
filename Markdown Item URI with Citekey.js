2391e9da288ccdf691353e4a1926363646c0a3ae
Silent <siltengen@gmail.com>
2020-11-20 16:33:43 +0100

    Fixed issue with Dates
    The Translator should not have any more problems with parsing the date.

--- Markdown Item URI with Citekey.js
+++ Markdown Item URI with Citekey.js
@@ -1,30 +1,29 @@
 {
-    "translatorID":"d90fd4af-9eeb-4232-9feb-8fd05bcf6f8b",
-    "translatorType":2,
-    "label":"Markdown Item URI with Citekey",
-    "creator":"Silent",
-    "target":"markdown",
-    "minVersion":"2.0",
-    "maxVersion":"",
-    "priority":200,
-    "inRepository":false,
-    "lastUpdated":"2020-10-14"
-    }
-     
-    
-
-        function doExport() {
-            var item;
-            while(item = Zotero.nextItem()) {
-                var date = new Date(item.date).getFullYear()
-        
-                var year = date && !isNaN(date) ? date + ". " : (typeof item.date == 'undefined'?  "" : item.date + ". ");
-                var library_id = item.libraryID ? item.libraryID : "";
-                var author_lastname = item.creators[0].lastName ? item.creators[0].lastName + ". " : "";
-                var title = item.title ? "*" + item.title + "*" : "";
-                var key = item.key;
-                var citekey = item.citationKey ? `[[${item.citationKey}]]` : "";
-        
-                Zotero.write(`[${author_lastname}${year}${title}](zotero://select/items/${library_id}_${key})${citekey}`)
-            }
-        }
+	"translatorID":"8dbf9b92-f796-4153-8f2e-ac6c157500dc",
+	"translatorType":2,
+	"label":"Markdown Item URI",
+	"creator":"Silent",
+	"target":"markdown",
+	"minVersion":"2.0",
+	"maxVersion":"",
+	"priority":200,
+	"inRepository":false,
+	"lastUpdated":"2020-10-15",
+	"local": ""
+}
+	 
+	
+	function doExport() {
+		var item;
+		while(item = Zotero.nextItem()) {
+			var date = Zotero.Utilities.strToDate(item.date).year;
+			var year = date && !isNaN(date) ? date + ". " : (typeof item.date == 'undefined'?  "" : item.date + ". ");
+			var library_id = item.libraryID ? item.libraryID : "";
+			var author_lastname = item.creators[0].lastName ? item.creators[0].lastName + ". " : "";
+			var title = item.title ? "*" + item.title + "*" : "";
+			var key = item.key;
+	
+			Zotero.write(`[${author_lastname}${year}${title}](zotero://select/items/${library_id}_${key})`)
+		}
+	}
+	
