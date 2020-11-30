bb0e399009f7861346821386e5820b3937b17bc8
silentdot <siltengen@gmail.com>
2020-11-30 16:30:33 +0100

    Fixed Problem with the Author Field

--- Markdown Item URI.js
+++ Markdown Item URI.js
@@ -8,7 +8,7 @@
 	"maxVersion":"",
 	"priority":200,
 	"inRepository":false,
-	"lastUpdated":"2020-10-15"
+	"lastUpdated":"2020-11-30"
 	}
 	 
 	
@@ -16,14 +16,23 @@
 		var item;
 		while(item = Zotero.nextItem()) {
 			var date = Zotero.Utilities.strToDate(item.date).year;
-	
+
 			var year = date && !isNaN(date) ? date + ". " : (typeof item.date == 'undefined'?  "" : item.date + ". ");
 			var library_id = item.libraryID ? item.libraryID : "";
-			var author_lastname = item.creators[0].lastName ? item.creators[0].lastName + ". " : "";
+			var author_lastname = getValidAuthor(item);
 			var title = item.title ? "*" + item.title + "*" : "";
 			var key = item.key;
 	
 			Zotero.write(`[${author_lastname}${year}${title}](zotero://select/items/${library_id}_${key})`);
 		}
+	}
+
+	function getValidAuthor(item){
+		if(item.creators && item.creators[0] && item.creators[0].lastName){
+			return item.creators[0].lastName + ". ";
+		}else{
+			return "";
+		}
+
 	}
 	
